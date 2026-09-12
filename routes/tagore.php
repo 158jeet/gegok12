d220b4450a637e575390158ce91f41add258d39f
158jeet <vishvajeet.sh@gmail.com>
2026-09-12 10:03:59 +0530

    feat: add unified Tagore child portal routes

--- routes/tagore.php
+++ routes/tagore.php
@@ -5,5 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->prefix('tagore')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('tagore.dashboard');
+    Route::get('/child/{studentId}', [DashboardController::class, 'child'])->whereNumber('studentId')->name('tagore.child');
+    Route::post('/child/{studentId}/feedback', [DashboardController::class, 'submitFeedback'])->whereNumber('studentId')->name('tagore.feedback.submit');
     Route::get('/api/dashboard', [DashboardController::class, 'api'])->name('tagore.dashboard.api');
 });
