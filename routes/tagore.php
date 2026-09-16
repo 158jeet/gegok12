a96d40ce963efb4407e62a107ec2831ccb6c0b3b
158jeet <vishvajeet.sh@gmail.com>
2026-09-16 13:47:25 +0530

    feat: wire fee import routes

--- routes/tagore.php
+++ routes/tagore.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Tagore\DashboardController;
 use App\Http\Controllers\Tagore\FeeController;
+use App\Http\Controllers\Tagore\FeeImportController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->prefix('tagore')->group(function () {
@@ -11,5 +12,8 @@ Route::middleware(['auth'])->prefix('tagore')->group(function () {
     Route::get('/child/{studentId}/fees', [FeeController::class, 'student'])->whereNumber('studentId')->name('tagore.fees.student');
     Route::get('/accounts/fees', [FeeController::class, 'accounts'])->name('tagore.fees.accounts');
     Route::post('/accounts/fees/student/{studentId}/payment', [FeeController::class, 'recordOfflinePayment'])->whereNumber('studentId')->name('tagore.fees.payment');
+    Route::get('/accounts/fees/import', [FeeImportController::class, 'index'])->name('tagore.fees.import');
+    Route::post('/accounts/fees/import/preview', [FeeImportController::class, 'preview'])->name('tagore.fees.import.preview');
+    Route::post('/accounts/fees/import/{batchId}/apply', [FeeImportController::class, 'apply'])->whereNumber('batchId')->name('tagore.fees.import.apply');
     Route::get('/api/dashboard', [DashboardController::class, 'api'])->name('tagore.dashboard.api');
 });
