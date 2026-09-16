f47100d46acffd67689159ab476acb45cab3aad8
158jeet <vishvajeet.sh@gmail.com>
2026-09-16 13:58:25 +0530

    test(tagore): cover fee management routes

--- tests/Feature/TagoreFeeImportTest.php
+++ tests/Feature/TagoreFeeImportTest.php
@@ -1,21 +1,47 @@
 <?php
+
 namespace Tests\Feature;
+
 use Tests\TestCase;
+
 class TagoreFeeImportTest extends TestCase
 {
-    public function test_fee_import_layer_files_exist(): void
+    public function test_fee_layer_files_exist(): void
     {
-        $this->assertFileExists(base_path('app/Services/Tagore/LegacyFeeImportService.php'));
-        $this->assertFileExists(base_path('app/Http/Controllers/Tagore/FeeImportController.php'));
-        $this->assertFileExists(base_path('resources/views/tagore/fees/import.blade.php'));
-        $this->assertFileExists(base_path('database/migrations/2026_09_16_120000_complete_tagore_fee_engine.php'));
-        $this->assertFileExists(base_path('database/migrations/2026_09_16_121000_create_tagore_fee_import_tables.php'));
+        foreach ([
+            'app/Services/Tagore/LegacyFeeImportService.php',
+            'app/Http/Controllers/Tagore/FeeImportController.php',
+            'app/Http/Controllers/Tagore/FeeManagementController.php',
+            'resources/views/tagore/fees/import.blade.php',
+            'resources/views/tagore/fees/manage.blade.php',
+            'database/migrations/2026_09_16_120000_complete_tagore_fee_engine.php',
+            'database/migrations/2026_09_16_121000_create_tagore_fee_import_tables.php',
+        ] as $path) {
+            $this->assertFileExists(base_path($path));
+        }
     }
-    public function test_fee_import_routes_are_present(): void
+
+    public function test_fee_routes_are_present(): void
     {
-        $routes=file_get_contents(base_path('routes/tagore.php'));
-        $this->assertStringContainsString('tagore.fees.import',$routes);
-        $this->assertStringContainsString('tagore.fees.import.preview',$routes);
-        $this->assertStringContainsString('tagore.fees.import.apply',$routes);
+        $routes = file_get_contents(base_path('routes/tagore.php'));
+        foreach ([
+            'tagore.fees.manage',
+            'tagore.fees.manage.structure',
+            'tagore.fees.manage.demand',
+            'tagore.fees.import',
+            'tagore.fees.import.preview',
+            'tagore.fees.import.apply',
+        ] as $route) {
+            $this->assertStringContainsString($route, $routes);
+        }
+    }
+
+    public function test_fee_service_contains_atomic_demand_and_payment_paths(): void
+    {
+        $service = file_get_contents(base_path('app/Services/Tagore/FeeService.php'));
+        $this->assertStringContainsString('createObligation', $service);
+        $this->assertStringContainsString('recordOfflinePayment', $service);
+        $this->assertStringContainsString('tagore_payment_allocations', $service);
+        $this->assertStringContainsString('DB::transaction', $service);
     }
 }
