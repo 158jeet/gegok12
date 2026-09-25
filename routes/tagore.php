01763592311bb5f965151f3c73b54f64983886a6
158jeet <vishvajeet.sh@gmail.com>
2026-09-25 20:21:36 +0530

    feat: add manager review route

--- routes/tagore.php
+++ routes/tagore.php
@@ -39,6 +39,7 @@ Route::middleware(['auth', \App\Http\Middleware\TagorePerformance::class])->pref
     Route::get('/tasks', [TaskController::class, 'index'])->name('tagore.tasks.index');
     Route::post('/tasks', [TaskController::class, 'store'])->name('tagore.tasks.store');
     Route::patch('/tasks/{taskId}', [TaskController::class, 'update'])->whereNumber('taskId')->name('tagore.tasks.update');
+    Route::post('/tasks/{taskId}/review', [TaskController::class, 'review'])->whereNumber('taskId')->name('tagore.tasks.review');
 
     Route::get('/child/{studentId}', [DashboardController::class, 'child'])->whereNumber('studentId')->name('tagore.child');
     Route::post('/child/{studentId}/feedback', [DashboardController::class, 'submitFeedback'])->whereNumber('studentId')->name('tagore.feedback.submit');
