2bc425e44fc7310ced438fbdfdb636e43d83da4f
158jeet <vishvajeet.sh@gmail.com>
2026-09-25 10:43:16 +0530

    feat(tagore): add admissions CRM data layer

---
+++ app/Models/TagoreAdmissionLead.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TagoreAdmissionLead extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'tagore_admission_leads';
+
+    protected $fillable = [
+        'institution_id','academic_year_id','lead_no','student_name','parent_name',
+        'mobile','alternate_mobile','email','class_name','source','status',
+        'assigned_to','next_follow_up_at','notes',
+    ];
+
+    protected $casts = ['next_follow_up_at' => 'datetime'];
+
+    public function activities()
+    {
+        return $this->hasMany(TagoreAdmissionActivity::class, 'lead_id');
+    }
+}
