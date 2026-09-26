696b607a744fe6162a909ec4a7808d9eeb4c5bf0
158jeet <vishvajeet.sh@gmail.com>
2026-09-26 11:32:32 +0530

    fix: keep Kobo sync and admissions import in one scheduled job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -34,7 +34,6 @@ class Kernel extends ConsoleKernel
             \App\Console\Commands\CheckTask::class,
             \App\Console\Commands\GenerateTagoreRecurringTasks::class,
             \App\Console\Commands\KoboSyncCommand::class,
-            \App\Console\Commands\KoboImportAdmissionsCommand::class,
 
             \App\Console\Commands\DataSeeder\SeedAttendance::class,
 
@@ -92,14 +91,6 @@ class Kernel extends ConsoleKernel
                      ->withoutOverlapping();
         }
 
-        if (config('kobo.api_token') && config('kobo.asset_uid') && config('kobo.institution_id')) {
-            $schedule->command('kobo:import-admissions')
-                     ->everyFiveMinutes()
-                     ->withoutOverlapping()
-                     ->after(function () {
-                         // Import runs against the synchronized local submissions table.
-                     });
-        }
 
         $schedule->command('gego:checktask')
                  ->everyMinute()
