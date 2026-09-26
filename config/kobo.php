5fdcae6fcfc8c6987aa01114a8d5f50882e23e28
158jeet <vishvajeet.sh@gmail.com>
2026-09-26 11:32:16 +0530

    feat: configure Kobo admission institution

--- config/kobo.php
+++ config/kobo.php
@@ -4,5 +4,6 @@ return [
     'base_url' => rtrim(env('KOBO_BASE_URL', 'https://kf.kobotoolbox.org'), '/'),
     'api_token' => env('KOBO_API_TOKEN'),
     'asset_uid' => env('KOBO_ASSET_UID'),
+    'institution_id' => env('KOBO_INSTITUTION_ID'),
     'timeout' => (int) env('KOBO_TIMEOUT', 30),
 ];
